f5de29f654c7f19456fc98aa21ae50edbbb51398
ri5hi-kumar <rishikumar10dec@gmail.com>
2023-04-07 22:48:03 +0530

    game logic added

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-const player = (sign) => {
-    this.sign = sign;
-    const getsign = () => {
-        return sign;
-    }
-
-    return {getsign};
-}
-
 const gameBoard = () => {
     const board = ["", "", "", "", "", "", "", "", ""];
 
     const setSign = (sign, position) => {
-        if(position > board.length) return;
+        if(position > board.length || board[position] !== "") return false;
         board[position] = sign;
+        return true;
     } 
 
     const getSign = (position) => {
@@ -26,5 +18,88 @@ const gameBoard = () => {
         }
     }
 
-    return {setSign, getSign, reset};
+    const printBoard = () => {
+        console.log(board[0] + " | " + board[1] + " | " + board[2]);
+        console.log("---------");
+        console.log(board[3] + " | " + board[4] + " | " + board[5]);
+        console.log("---------");
+        console.log(board[6] + " | " + board[7] + " | " + board[8]);
+    }
+
+
+    return {setSign, getSign, reset, printBoard};
+}
+
+
+const gameController = (currentBoard) => {
+    let isGameOver = false;
+    let player = 'X';
+    let turn = 1;
+
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 5]
+    ];
+
+    const checkWiningCodition = () => {
+        for(let i = 0; i < winConditions.length; i++){
+            const gameState = winConditions[i];
+            let a = currentBoard.getSign(gameState[0]);
+            let b = currentBoard.getSign(gameState[1]);
+            let c = currentBoard.getSign(gameState[2]);
+
+            if(a === player && (a === b && b === c)){
+                isGameOver = true;
+                console.log(`${player} won!`);
+                break;
+            }
+        }
+    }
+
+    const changePlayer = () => {
+        player = player === 'X' ? 'O' : 'X';
+    }
+
+    const getGameState = () => {
+        return isGameOver;
+    }
+
+    const playTurn = (index) => {
+        if(turn === 9) {
+            console.log('DRAW');
+            isGameOver = true;
+            return;
+        }
+        if(!currentBoard.setSign(player, index)){
+            return;
+        }
+        checkWiningCodition();
+        changePlayer();
+        turn++;
+    }
+
+    const resetgame = () => {
+        isGameOver = false;
+        currentBoard.reset();
+    }
+
+    return {playTurn, getGameState, resetgame};
+}
+
+
+// execution of the game starts here
+
+const currentBoard = gameBoard();
+const game = gameController(currentBoard);
+
+while(!game.getGameState()){
+    const index = prompt("Enter the index where you want to play");
+    game.playTurn(index);
+    currentBoard.printBoard();
 }
